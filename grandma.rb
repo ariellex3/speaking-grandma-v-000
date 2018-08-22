39e967e025fa93fe5ae20187052df1edba913b17
Arielle D <domingu4@gmail.com>
2018-08-22 13:36:30 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,11 @@
-def speak_to_grandma
+def speak_to_grandma(phrase)
+  if phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
 end
 
 # Whatever you say to grandma, she should respond with
